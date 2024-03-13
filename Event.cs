8097f3033f3ead3295cd0074a36955d1278d4bfc
Bstrinic <BogdanStrinic003@gmail.com>
2024-03-13 17:00:45 -0600

    Add project files.

---
+++ Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_CPRG213
+{
+    internal class Event
+    {
+        public int EventNumber { get; set; }
+        public string? Location { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab6_CPRG213
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an object
+            Event myObj = new Event();
+
+            // Assigning the values
+            myObj.EventNumber = 1;
+            myObj.Location = "Calgary";
+
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
